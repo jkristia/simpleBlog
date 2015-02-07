d60328688ba3ad92c9521a08203759cf3b4aec57
Jesper Kristiansen <jkristia@outlook.com>
2015-02-06 21:45:29 -0800

    added controller

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
 
 namespace SimpleBlog
 {
@@ -12,12 +13,11 @@ namespace SimpleBlog
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-			routes.MapRoute(
-				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
+			
+			// this to point to a specific Posts controller
+			string[] namespaces = new string[] {typeof(PostsController).Namespace};
+			routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"}, namespaces);
+			routes.MapRoute("Home", "", new {controller = "Posts", action = "Index"}, namespaces);
 		}
 	}
 }
