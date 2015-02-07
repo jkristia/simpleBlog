dfc5dc759e0ab852d9e4eb2cca15440f26bfa149
Jesper Kristiansen <jkristia@outlook.com>
2015-02-07 14:14:37 -0800

    test commit

--- Areas/Admin/Controllers/UsersController.cs
+++ Areas/Admin/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
-            return Content("Users");
+            return Content("Users - 1");
         }
     }
 }
