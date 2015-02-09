097450aa9750b6076d505e187a1f31879234d50f
Jesper Kristiansen <jkristia@outlook.com>
2015-02-08 17:59:23 -0800

    basic admin layout

---
+++ Infrastructure/SelectedTabAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Infrastructure
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class SelectedTabAttribute : ActionFilterAttribute
+	{
+		public string SelectedTab { get; private set; }
+		public SelectedTabAttribute(string selectedTab)
+		{
+			SelectedTab = selectedTab;
+		}
+		public override void OnResultExecuting(ResultExecutingContext filterContext)
+		{
+			filterContext.Controller.ViewBag.SelectedTab = SelectedTab;
+		}
+	}
+}
